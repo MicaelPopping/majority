c0dfed7f0bd0c35cdb61b5dab4fd1ed075cee294
Micael Popping <mmpopping@inf.ufpel.edu.br>
2019-11-02 20:48:23 -0300

    att brc_manager

--- src/brc/brc_manager.hpp
+++ src/brc/brc_manager.hpp
@@ -2,6 +2,16 @@
 #define BRC_MANAGER_HPP
 
 
+/**
+ * This class manage the operations with BR_Code.
+ * @author Micael Popping.
+*/
+class BRC_Manager {
 
+private:
+
+public:
+
+}
 
 #endif //BRC_MANAGER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@ using std::endl;
 int main(int argc, char **argv) {
 
     cout << "Majority" << endl;
+    
     return 0;
 }
