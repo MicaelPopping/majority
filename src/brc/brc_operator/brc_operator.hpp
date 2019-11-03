f560336aeeba5536ab5644ae6e12dad4ffdda00e
Micael Popping <mmpopping@inf.ufpel.edu.br>
2019-11-03 11:28:03 -0300

    Resolved the types brc_operator.hpp

--- src/brc/brc_operator/brc_operator.hpp
+++ src/brc/brc_operator/brc_operator.hpp
@@ -2,19 +2,21 @@
 #define BRC_OPERATOR_HPP
 
 
-include "brc/br_code/br_code.hpp"
+#include <cstdint>
+
 #include "brc/brc_operations.hpp"
+#include "brc/br_code/br_code.hpp"
 
 
 class BRC_Operator : public BRC_Operations {
 
 protected:
-    const unsigned int NUMBER_OF_INPUTS;
-    const unsigned int VECTOR_SIZE;
+    const uint8_t INPUTS_NUMBER;
+    const uint8_t VECTOR_SIZE;
 
     
 public:
-    BRC_Operator(unsigned int number_of_inputs, unsigned int vector_size);
+    BRC_Operator(uint8_t inputs_number,  uint8_t vector_size);
     virtual ~BRC_Operator();
 
     BR_Code* execute_and(BR_Code& operating_1, BR_Code& operating_2);
@@ -22,9 +24,9 @@ public:
     virtual BR_Code* execute_not(BR_Code& operating_1) = 0;
     BR_Code* execute_maj3(BR_Code& operating_1, BR_Code& operating_2, BR_Code& operating_3);
 
-    virtual BR_Code* primary_value_at(int level) = 0;
+    virtual BR_Code* primary_value_at(uint8_t level) = 0;
 
-    unsigned int get_vector_size();
+    uint8_t get_vector_size();
 };
 
 #endif //BRC_OPERATOR_HPP
