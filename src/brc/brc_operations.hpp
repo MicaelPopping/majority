b8f760703878de49ce4f2a52067dca09f44c8264
Micael Popping <mmpopping@inf.ufpel.edu.br>
2019-11-03 11:16:18 -0300

    Att brc_operations.hpp brc_operator.hpp

--- src/brc/brc_operations.hpp
+++ src/brc/brc_operations.hpp
@@ -1,9 +1,17 @@
 #ifndef BRC_OPERATIONS_HPP
 #define BRC_OPERATIONS_HPP
 
+
+#include "brc/br_code/br_code.hpp"
+
+
 class BRC_Operations {
 
 public:
+    virtual BR_Code* execute_and(BR_Code& operating_1, BR_Code& operating_2) = 0;
+    virtual BR_Code* execute_or(BR_Code& operating_1, BR_Code& operating_2) = 0;
+    virtual BR_Code* execute_not(BR_Code& operating_1) = 0;
+    virtual BR_Code* execute_maj3(BR_Code& operating_1, BR_Code& operating_2, BR_Code& operating_3) = 0;
 };
 
 #endif //BRC_OPERATIONS_HPP
--- src/brc/brc_operator/brc_operator.hpp
+++ src/brc/brc_operator/brc_operator.hpp
@@ -2,16 +2,29 @@
 #define BRC_OPERATOR_HPP
 
 
-#include "brc/br_code/br_code.hpp"
+include "brc/br_code/br_code.hpp"
+#include "brc/brc_operations.hpp"
 
 
-class BRC_Operations {
+class BRC_Operator : public BRC_Operations {
 
+protected:
+    const unsigned int NUMBER_OF_INPUTS;
+    const unsigned int VECTOR_SIZE;
+
+    
 public:
-    virtual BR_Code* execute_and(BR_Code& operating_1, BR_Code& operating_2) = 0;
-    virtual BR_Code* execute_or(BR_Code& operating_1, BR_Code& operating_2) = 0;
+    BRC_Operator(unsigned int number_of_inputs, unsigned int vector_size);
+    virtual ~BRC_Operator();
+
+    BR_Code* execute_and(BR_Code& operating_1, BR_Code& operating_2);
+    BR_Code* execute_or(BR_Code& operating_1, BR_Code& operating_2);
     virtual BR_Code* execute_not(BR_Code& operating_1) = 0;
-    virtual BR_Code* execute_maj3(BR_Code& operating_1, BR_Code& operating_2, BR_Code& operating_3) = 0;
+    BR_Code* execute_maj3(BR_Code& operating_1, BR_Code& operating_2, BR_Code& operating_3);
+
+    virtual BR_Code* primary_value_at(int level) = 0;
+
+    unsigned int get_vector_size();
 };
 
 #endif //BRC_OPERATOR_HPP
