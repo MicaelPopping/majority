8a0a18ac39e6b8e088857547fbf58d9662242e0a
Micael Popping <mmpopping@inf.ufpel.edu.br>
2019-11-04 13:36:56 -0300

    Start brc_operator_short

---
+++ src/brc/brc_operator/brc_operator_short.hpp
@@ -0,0 +1,25 @@
+#ifndef BRC_OPERATOR_SHORT_HPP
+#define BRC_OPERATOR_SHORT_HPP
+
+
+#include <cstdint>
+
+#include "brc/brc_operator/brc_operator.hpp"
+
+
+class BRC_Operator_Short : public BRC_Operator {
+
+public:
+    BRC_Operator_Short(uint8_t number_of_inputs);
+    virtual ~BRC_Operator_Short();
+
+    BR_Code* execute_not(BR_Code& operating_1);
+
+    virtual BR_Code* primary_value_at(uint8_t level) = 0;
+
+protected:
+    void convert_dec_to_bin(uint64_t number_dec, char* number_bin, uint8_t size);
+    uint64_t convert_bin_to_dec(char* number_bin, uint8_t size);
+};
+
+#endif //BRC_OPERATOR_SHORT_HPP
