f9e55159892d11398aca0216e84196cae08d06e8
Micael Popping <mmpopping@inf.ufpel.edu.br>
2019-11-04 12:59:11 -0300

    brc_manager attributes

--- src/brc/brc_manager.hpp
+++ src/brc/brc_manager.hpp
@@ -2,6 +2,16 @@
 #define BRC_MANAGER_HPP
 
 
+#include <cstdint>
+#include <map>
+using std::map;
+#include <string>
+using std::string;
+
+#include "brc/br_code/br_code.hpp"
+#include "brc/brc_operator/brc_operator.hpp"
+
+
 /**
  * This class manage the operations with BR_Code.
  * @author Micael Popping.
@@ -9,6 +19,11 @@
 class BRC_Manager {
 
 private:
+    uint8_t inputs_number;
+    map<string, BR_Code*> table;
+    BR_Code *const_true;
+    BR_Code *const_false;
+    BRC_Operator* brc_operator;
 
 public:
 
