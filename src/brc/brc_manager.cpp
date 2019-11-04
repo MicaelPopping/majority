71a602bda91aca060d47ec5000599f4ac9b4d208
Micael Popping <mmpopping@inf.ufpel.edu.br>
2019-11-04 13:25:56 -0300

    Att class  brc_manager

--- src/brc/brc_manager.cpp
+++ src/brc/brc_manager.cpp
@@ -1,1 +1,124 @@
 #include "brc/brc_manager.hpp"
+
+
+#include <utility>
+using std::pair;
+
+#include <iterator>
+using std::iterator;
+
+
+/************************************************************************
+*                           Publics methods                             *
+************************************************************************/
+
+BRC_Manager::BRC_Manager()
+: inputs_number(0), const_true(nullptr), const_false(nullptr), brc_operator(nullptr) {}
+
+/************************************************************************/
+
+BRC_Manager::BRC_Manager(set<string>& inputs) {
+
+    generate_values(inputs);
+}
+
+/************************************************************************/
+
+BRC_Manager::~BRC_Manager() {
+
+    // IMPLEMENTAR DESTRUTOR.
+}
+
+/************************************************************************/
+
+void BRC_Manager::insert(string input) {
+
+    table.insert(pair<string, BR_Code*> (input, nullptr));
+    inputs_number = table.size();
+}
+
+/************************************************************************/
+
+void BRC_Manager::generate_values() {
+
+    if(inputs_number == 0)
+        return;
+
+    //initialize_operator();
+    start_values();
+}
+
+/************************************************************************/
+
+void BRC_Manager::generate_values(set<string>& inputs) {
+
+    inputs_number = inputs.size();
+
+    for(set<string>::iterator i = inputs.begin(); i != inputs.end(); i++) {
+        table.insert(pair<string, BR_Code*> (*i, nullptr));
+    }
+
+    generate_values();
+}
+
+/************************************************************************/
+
+BR_Code* BRC_Manager::brc_input_of(string input) {
+
+    return table.find(input)->second;
+}
+
+/************************************************************************/
+
+BR_Code* BRC_Manager::execute_and(BR_Code& operating_1, BR_Code& operating_2) {
+
+    return brc_operator->execute_and(operating_1, operating_2);
+}
+
+/************************************************************************/
+
+BR_Code* BRC_Manager::execute_or(BR_Code& operating_1, BR_Code& operating_2) {
+
+    return brc_operator->execute_or(operating_1, operating_2);
+}
+
+/************************************************************************/
+
+BR_Code* BRC_Manager::execute_not(BR_Code& operating_1) {
+
+    return brc_operator->execute_not(operating_1);
+}
+
+/************************************************************************/
+
+BR_Code* BRC_Manager::execute_maj3(BR_Code& operating_1, BR_Code& operating_2, BR_Code& operating_3) {
+
+    return execute_maj3(operating_1, operating_2, operating_3);
+}
+
+/************************************************************************
+*                         Protecteds methods                            *
+************************************************************************/
+
+/************************************************************************
+*                          Privates methods                             *
+************************************************************************/
+
+void BRC_Manager::initialize_operator() {
+
+    // IMPLEMENTAR METODO.
+}
+
+/************************************************************************/
+
+ void BRC_Manager::start_values() {
+
+     uint8_t level = 1;
+
+     for(map<string, BR_Code*>::iterator i = table.begin(); i != table.end(); i++, level++)
+        i->second = brc_operator->primary_value_at(level);
+}
+
+/************************************************************************
+*                           Statics methods                             *
+************************************************************************/
--- src/brc/brc_manager.hpp
+++ src/brc/brc_manager.hpp
@@ -7,7 +7,10 @@
 using std::map;
 #include <string>
 using std::string;
+#include <set>
+using std::set;
 
+#include "brc/brc_operations.hpp"
 #include "brc/br_code/br_code.hpp"
 #include "brc/brc_operator/brc_operator.hpp"
 
@@ -16,7 +19,7 @@ using std::string;
  * This class manage the operations with BR_Code.
  * @author Micael Popping.
 */
-class BRC_Manager {
+class BRC_Manager : BRC_Operations {
 
 private:
     uint8_t inputs_number;
@@ -26,7 +29,48 @@ private:
     BRC_Operator* brc_operator;
 
 public:
+    BRC_Manager();
+    BRC_Manager(set<string>& inputs);
+    ~BRC_Manager();
 
+    /**
+     * @param input
+    */
+    void insert(string input);
+
+    /**
+     * These methods assign values to the inputs.
+    */
+    //void generate_values();
+
+    /**
+     * @param set containing the inputs.
+    */
+    //void generate_values(set<string>& inputs);
+
+    /**
+     * 
+    */
+    BR_Code* brc_input_of(string input);
+
+    /**
+     * BRC_Operations methods
+    */
+    BR_Code* execute_and(BR_Code& operating_1, BR_Code& operating_2);
+    BR_Code* execute_or(BR_Code& operating_1, BR_Code& operating_2);
+    BR_Code* execute_not(BR_Code& operating_1);
+    BR_Code* execute_maj3(BR_Code& operating_1, BR_Code& operating_2, BR_Code& operating_3);
+
+private:
+    /**
+     * This method defines the operator to use.
+    */
+    void initialize_operator();
+
+    /**
+     * This method asks the operator of the inputs values.
+    */
+    void start_values();
 };
 
 #endif //BRC_MANAGER_HPP
