072f820bda90fff392e953704fa5f4a152d969db
Micael Popping <mmpopping@inf.ufpel.edu.br>
2019-11-07 19:19:22 -0300

    main - brc

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,19 @@
 using std::cout;
 using std::endl;
 
+#include "brc/brc_manager.hpp"
+
+
 int main(int argc, char **argv) {
 
     cout << "Majority" << endl;
+    BRC_Manager brc;
 
+    brc.insert("x1");
+    brc.insert("x2");
+    brc.insert("x3");
+
+    brc.generate_values();
+    
     return 0;
 }
